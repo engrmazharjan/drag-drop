fad2595f74d67f6dfb15385a8dcb8fe01541aef9
Mazhar Jan <60063589+engrmazharjan@users.noreply.github.com>
2023-05-18 18:16:56 +0500

    Code Refactoring & ES Modules

---
+++ dist/decorators/autobind.js
@@ -0,0 +1,12 @@
+// * AutoBind Decorator
+export const AutoBind = (_, _2, descriptor) => {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjustedDescriptor;
+};
--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,18 +1,16 @@
-namespace App {
-  // * AutoBind Decorator
-  export const AutoBind = (
-    _: any,
-    _2: string,
-    descriptor: PropertyDescriptor
-  ) => {
-    const originalMethod = descriptor.value;
-    const adjustedDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjustedDescriptor;
+// * AutoBind Decorator
+export const AutoBind = (
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) => {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
   };
-}
+  return adjustedDescriptor;
+};
